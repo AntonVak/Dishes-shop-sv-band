3a65c098b63d0d974be2d925d3171eefe795d9b9
AntonVak <antonv.snab@gmail.com>
2023-03-10 16:04:03 +0200

    add counter, counterList

--- src/components/Counter/counter.js
+++ src/components/Counter/counter.js
@@ -4,21 +4,33 @@ import '../../App.css';
 
 
 const Counter = (props) => {
-    
+    const {handlerInc, value} = props;
+
+    let clazz = value === 0?'danger' : 'primary' 
+    const formatCount = () => {
+      return value ===0 ? "empty" : value
+    }
+    const handlerIncrement = () => {
+      handlerInc(props.id)
+    }
+
+    const handleDelet = () => {
+      props.onDelete(props.id)
+    }
    
     return (
       <div className="app">
-        <h2 className='name'></h2>
+        <h2 className='name'>{props.name}</h2>
         <h3>
-          <span className={`count badge m-2`}></span>
+          <span className={`count badge m-2 bg-${clazz}`}>{formatCount()}</span>
         </h3>
-        <button className="btn btn-primary btn-lg m-2" >
+        <button className="btn btn-primary btn-lg m-2" onClick={handlerIncrement}>
           +
         </button>
         <button className="btn btn-primary btn-lg m-2" >
           -
         </button>
-        <button className="btn btn-danger btn-lg m-2" >Delet</button>
+        <button className="btn btn-danger btn-lg m-2" onClick={handleDelet} >Delet</button>
       </div>
     );
 }
--- src/components/CounterList/counterList.js
+++ src/components/CounterList/counterList.js
@@ -8,31 +8,48 @@ const CounterList = () => {
         { id: 1, value: 0, name: 'Вилка' },
         { id: 2, value: 0, name: 'Тарелка' },
         { id: 3, value: 0, name: 'Набор посуды' },
-      ]
+    ]
+  const [counters, setCounters] = useState(initialState);
      
 
     const handleDelet = (id) => {
-       
+       const itemDel = counters.filter((item) => item.id !== id);
+       setCounters(itemDel);
     }
     const handleReset = () => {
-       
+       setCounters(initialState)
     }
 
     const handlerInc = (id) => {
-     
+      //поиск элемента по индексу
+      const elementIndex = counters.findIndex(ind => ind.id ===id);
+      //создадим новый массив который буде содержать текущее состояние
+      const newCounters = [...counters];
+      newCounters[elementIndex].value++
+      setCounters(newCounters)
     }
     const handlerDec = (id) => {
-       
+      const elementIndex = counters.findIndex(ind => ind.id ===id);
+      //создадим новый массив который буде содержать текущее состояние
+      const newCounters = [...counters];
+      newCounters[elementIndex].value--
+      setCounters(newCounters)
     }
     return (
       <>
-        
-          <Counter 
-          onDelete={handleDelet}
-          handlerInc={handlerInc}
-          handlerDec={handlerDec} />
-        
-        <button className="btn btn-danger btn-lg m-2">Reset</button>
+        {counters.map((count) => (
+          <Counter
+            key={count.id}
+            onDelete={handleDelet}
+            handlerInc={handlerInc}
+            handlerDec={handlerDec}
+            {...count}
+          />
+        ))}
+
+        <button className="btn btn-danger btn-lg m-2" onClick={handleReset}>
+          Reset
+        </button>
       </>
     );
 }
